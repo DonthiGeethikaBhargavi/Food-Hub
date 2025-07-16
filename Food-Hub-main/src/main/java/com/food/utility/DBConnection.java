61c26b9c02808a41331717cc6b5a700613ee2d62
DonthiGeethikaBhargavi <149235307+DonthiGeethikaBhargavi@users.noreply.github.com>
2025-07-16 12:13:37 +0530

    DBConnection.java

--- Food-Hub-main/src/main/java/com/food/utility/DBConnection.java
+++ Food-Hub-main/src/main/java/com/food/utility/DBConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DBConnection {
     private static final String URL = System.getenv("DB_URL");
     private static final String USERNAME = System.getenv("DB_USERNAME");
-    private static final String PASSWORD = System.getenv("DB_USERNAME");
+    private static final String PASSWORD = System.getenv("DB_PASSWORD");
 
     public static Connection getConnection() {
         Connection connection = null;

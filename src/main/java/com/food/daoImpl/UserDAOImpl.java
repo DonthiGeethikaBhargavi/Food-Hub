424f84d9709eb77a2d4ff75b27c23c810ff0590e
DonthiGeethikaBhargavi <149235307+DonthiGeethikaBhargavi@users.noreply.github.com>
2025-07-20 10:17:40 +0530

    Updated UserDAOImpl.java

--- src/main/java/com/food/daoImpl/UserDAOImpl.java
+++ src/main/java/com/food/daoImpl/UserDAOImpl.java
@@ -1,5 +1,5 @@
 package com.food.daoImpl;
-
+import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.sql.*;
@@ -141,18 +141,19 @@ public class UserDAOImpl implements UserDAO {
     }
 
     private String hashPassword(String password) {
-        try {
-            MessageDigest md = MessageDigest.getInstance("SHA-256");
-            byte[] hashedBytes = md.digest(password.getBytes());
-            StringBuilder sb = new StringBuilder();
-            for (byte b : hashedBytes) {
-                sb.append(String.format("%02x", b));
-            }
-            return sb.toString();
-        } catch (NoSuchAlgorithmException e) {
-            throw new RuntimeException("Error hashing password", e);
+    try {
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+        byte[] hashedBytes = md.digest(password.getBytes(StandardCharsets.UTF_8)); 
+        StringBuilder sb = new StringBuilder();
+        for (byte b : hashedBytes) {
+            sb.append(String.format("%02x", b));
         }
+        return sb.toString();
+    } catch (NoSuchAlgorithmException e) {
+        throw new RuntimeException("Error hashing password", e);
     }
+}
+
     @Override
     public List<User> getUsersByRole(String role) {
         List<User> usersList = new ArrayList<>();
